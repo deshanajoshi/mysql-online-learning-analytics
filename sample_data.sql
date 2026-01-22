84ea5807e2c4c4ef0609fd7ef30b16801e7dced1
Deshna Joshi <joshideshna@gmail.com>
2026-01-22 21:28:34 +0530

    Create sample_data.sql

---
+++ sample_data.sql
@@ -0,0 +1,13 @@
+INSERT INTO students VALUES
+(1,'Aman'),(2,'Riya');
+
+INSERT INTO instructors VALUES
+(101,'Kunal'),(102,'Pooja');
+
+INSERT INTO courses VALUES
+(201,'SQL Analytics',101),
+(202,'Power BI Basics',102);
+
+INSERT INTO enrollments VALUES
+(1,1,201,'2024-02-01'),
+(2,2,202,'2024-02-05');
